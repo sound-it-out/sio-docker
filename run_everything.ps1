c46d8b92595c5334e0a3e6c7fac2427241c11303
matt <matthew.hope396@outlook.com>
2021-12-03 23:37:59 +0000

    wip

--- run_everything.ps1
+++ run_everything.ps1
@@ -1,10 +1,15 @@
 docker network create sio.network;
-docker-compose up -d
-docker-compose `
-    -f "docker-compose.yml" `
-    -f "docker-compose.override.yml" `
-    -f "docker-compose.store.yml" `
-    -f "docker-compose.store.override.yml" `
-    -f "docker-compose.cache.yml" `
-    -f "docker-compose.cache.override.yml" `
-    up -d
+$eventpublisherArgs = "-f ""../sio-eventpublisher/docker-compose.yml"" -f ""../sio-eventpublisher/docker-compose.override.yml""";
+$identityArgs = "-f ""../sio-identity/docker-compose.yml"" -f ""../sio-identity/docker-compose.override.yml""";
+$apiArgs = "-f ""../sio-api/docker-compose.yml"" -f ""../sio-api/docker-compose.override.yml""";
+$translationOptionImporter = "-f ""../sio-translation-option-importer/docker-compose.yml"" -f ""../sio-translation-option-importer/docker-compose.override.yml""";
+$frontArgs = "-f ""../sio-front/docker-compose.yml"" -f ""../sio-front/docker-compose.override.yml""";
+
+# Invoke-Expression "docker-compose $eventpublisherArgs $identityArgs $apiArgs $translationOptionImporter build --parallel"
+
+Invoke-Expression "docker-compose -f ""docker-compose.yml"" -f ""docker-compose.override.yml"" -p ""sio-shared"" up --no-deps -d --remove-orphans";
+Invoke-Expression "docker-compose $eventpublisherArgs -p ""sio-eventpublisher"" up --no-deps -d --remove-orphans";
+Invoke-Expression "docker-compose $identityArgs -p ""sio-identity"" up --no-deps -d --remove-orphans";
+Invoke-Expression "docker-compose $apiArgs -p ""sio-api"" up --no-deps -d --remove-orphans";
+Invoke-Expression "docker-compose $translationOptionImporter -p ""sio-translation-option-importer"" up --no-deps -d --remove-orphans";
+Invoke-Expression "docker-compose $frontArgs -p ""sio-front"" up --no-deps -d --remove-orphans";
