c72486a99e26a49ce8ae640d17df3dbb10966802
matt <matthew.hope396@outlook.com>
2022-01-06 23:22:27 +0000

    WIP

--- run_everything.ps1
+++ run_everything.ps1
@@ -3,12 +3,16 @@ $eventpublisherArgs = "-f ""../sio-eventpublisher/docker-compose.yml"" -f ""../s
 $identityArgs = "-f ""../sio-identity/docker-compose.yml"" -f ""../sio-identity/docker-compose.override.yml""";
 $apiArgs = "-f ""../sio-api/docker-compose.yml"" -f ""../sio-api/docker-compose.override.yml""";
 $translationOptionImporter = "-f ""../sio-translation-option-importer/docker-compose.yml"" -f ""../sio-translation-option-importer/docker-compose.override.yml""";
+$eventNotifierArgs = "-f ""../sio-eventnotifier/docker-compose.yml"" -f ""../sio-eventnotifier/docker-compose.override.yml""";
 $frontArgs = "-f ""../sio-front/docker-compose.yml"" -f ""../sio-front/docker-compose.override.yml""";
+$mailerArgs = "-f ""../sio-mailer/docker-compose.yml"" -f ""../sio-mailer/docker-compose.override.yml""";
 
 # Invoke-Expression "docker-compose $eventpublisherArgs $identityArgs $apiArgs $translationOptionImporter build --parallel"
 
 Invoke-Expression "docker-compose -f ""docker-compose.yml"" -f ""docker-compose.override.yml"" -p ""sio-shared"" up --no-deps -d --remove-orphans";
 Invoke-Expression "docker-compose $eventpublisherArgs -p ""sio-eventpublisher"" up --no-deps -d --remove-orphans";
+Invoke-Expression "docker-compose $eventNotifierArgs -p ""sio-eventnotifier"" up --no-deps -d --remove-orphans";
+Invoke-Expression "docker-compose $mailerArgs -p ""sio-mailer"" up --no-deps -d --remove-orphans";
 Invoke-Expression "docker-compose $identityArgs -p ""sio-identity"" up --no-deps -d --remove-orphans";
 Invoke-Expression "docker-compose $apiArgs -p ""sio-api"" up --no-deps -d --remove-orphans";
 Invoke-Expression "docker-compose $translationOptionImporter -p ""sio-translation-option-importer"" up --no-deps -d --remove-orphans";
